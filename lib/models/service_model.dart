c1c8f2f98517c30c72905b8900b296e03b2e3e9c
Hieu Bui Minh <hieubm1707@gmail.com>
2024-10-16 09:44:42 +0700

    feat: add service model

---
+++ lib/models/service_model.dart
@@ -0,0 +1,10 @@
+class Service {
+  const Service({
+    required this.path,
+    required this.name,
+    required this.value,
+  });
+  final String path;
+  final String name;
+  final bool value;
+}
