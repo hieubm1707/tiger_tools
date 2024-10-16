62711c3992ab5544e5bc4e8704f58008f3032782
Hieu Bui Minh <hieubm1707@gmail.com>
2024-10-16 10:20:07 +0700

    feat: adjust usage

--- bin/tiger.dart
+++ bin/tiger.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:args/args.dart';
 import 'package:tiger_tools/tiger_tools_lib.dart';
 
@@ -6,15 +8,21 @@ import 'service_config.dart';
 const String version = '0.0.1';
 
 void printUsage(ArgParser argParser) {
-  print('Usage: tiger <flags> [arguments]');
-  print(argParser.usage);
+  stdout.writeln('Hello I\'m Tiger Tools! \n');
+  stdout.writeln('Usage: tiger <command> [arguments]\n');
+  stdout.writeln('Global options:');
+  stdout.writeln('${argParser.usage}\n');
+  stdout.writeln('Available commands:');
+  for (final object in argParser.commands.entries) {
+    stdout.writeln('  ${object.key}         ${object.value.usage}');
+  }
+  // stdout.writeln(argParser.commands.keys);
 }
 
 void main(List<String> arguments) {
   final ArgParser argParser = buildParser();
   try {
     final ArgResults results = argParser.parse(arguments);
-    bool verbose = false;
 
     // Process the parsed arguments.
     if (results.wasParsed(Flag.help.name)) {
@@ -22,27 +30,24 @@ void main(List<String> arguments) {
       return;
     }
     if (results.wasParsed(Flag.version.name)) {
-      print('tiger tools version: $version');
-      return;
-    }
-    if (results.wasParsed(Flag.config.name)) {
-      print('Service Config');
-      buildServiceConfig();
+      stdout.writeln('tiger tools version: $version');
       return;
     }
     if (results.wasParsed(Flag.verbose.name)) {
-      verbose = true;
+      stdout.writeln('[VERBOSE] All arguments: ${results.arguments}');
+      printUsage(argParser);
+      return;
     }
-
-    // Act on the arguments provided.
-    print('Positional arguments: ${results.rest}');
-    if (verbose) {
-      print('[VERBOSE] All arguments: ${results.arguments}');
+    final command = results.command;
+    if (command?.name == Command.serviceConfig.value) {
+      stdout.writeln('Service Config');
+      buildServiceConfig();
+      return;
     }
   } on FormatException catch (e) {
     // Print usage information if an invalid argument was provided.
-    print(e.message);
-    print('');
+    stdout.writeln(e.message);
+    stdout.writeln('');
     printUsage(argParser);
   }
 }
