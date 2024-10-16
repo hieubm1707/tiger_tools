083525007a41f65151655c908b1fab36888609e2
Hieu Bui Minh <hieubm1707@gmail.com>
2024-10-16 17:08:56 +0700

    feat(mason): adjust to use command runner

--- bin/tiger.dart
+++ bin/tiger.dart
@@ -1,55 +1,17 @@
 import 'dart:io';
 
-import 'package:args/args.dart';
-import 'package:tiger_tools/tiger_tools_lib.dart';
-
-import 'service_config.dart';
-
-const String version = '0.0.1';
-
-void printUsage(ArgParser argParser) {
-  stdout.writeln('Hello I\'m Tiger Tools! \n');
-  stdout.writeln('Usage: tiger <command> [arguments]\n');
-  stdout.writeln('Global options:');
-  stdout.writeln('${argParser.usage}\n');
-  stdout.writeln('Available commands:');
-  for (final object in argParser.commands.entries) {
-    stdout.writeln('  ${object.key}         ${object.value.usage}');
-  }
-  // stdout.writeln(argParser.commands.keys);
-}
+import 'package:args/command_runner.dart';
+import 'package:tiger_tools/core/command/mason/mason_command.dart';
+import 'package:tiger_tools/core/command/service_config_command.dart';
 
 void main(List<String> arguments) {
-  final ArgParser argParser = buildParser();
-  try {
-    final ArgResults results = argParser.parse(arguments);
+  final runner = CommandRunner('tiger', 'Hello I\'m Tiger Tools! \n')
+    ..addCommand(ServiceConfigCommand())
+    ..addCommand(MasonCommand());
 
-    // Process the parsed arguments.
-    if (results.wasParsed(Flag.help.name)) {
-      printUsage(argParser);
-      return;
-    }
-    if (results.wasParsed(Flag.version.name)) {
-      stdout.writeln('tiger tools version: $version');
-      return;
-    }
-    if (results.wasParsed(Flag.verbose.name)) {
-      stdout.writeln('[VERBOSE] All arguments: ${results.arguments}');
-      printUsage(argParser);
-      return;
-    }
-    final command = results.command;
-    if (command?.name == Command.serviceConfig.value) {
-      stdout.writeln('Service Config');
-      buildServiceConfig();
-      return;
-    }
-    printUsage(argParser);
-    return;
-  } on FormatException catch (e) {
-    // Print usage information if an invalid argument was provided.
-    stdout.writeln(e.message);
-    stdout.writeln('');
-    printUsage(argParser);
-  }
+  runner.run(arguments).catchError((error) {
+    if (error is! UsageException) throw error;
+    print(error);
+    exit(64); // Exit code 64 indicates a usage error.
+  });
 }
