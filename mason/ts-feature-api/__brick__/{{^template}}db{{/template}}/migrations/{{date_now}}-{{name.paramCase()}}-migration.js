8be5004ebdb21cfee673dc10a860d938211fda49
Hieu Bui Minh <hieubm1707@gmail.com>
2024-12-04 15:07:44 +0700

    feat: add migration attributes into ts api feature mason

--- mason/ts-feature-api/__brick__/{{^template}}db{{/template}}/migrations/{{date_now}}-{{name.paramCase()}}-migration.js
+++ mason/ts-feature-api/__brick__/{{^template}}db{{/template}}/migrations/{{date_now}}-{{name.paramCase()}}-migration.js
@@ -6,38 +6,6 @@ module.exports = {
       '{{table_name}}',
       {
         // insert columns here
-        id: {
-          type: Sequelize.UUID,
-          primaryKey: true,
-          allowNull: false,
-          defaultValue: Sequelize.UUIDV4,
-          field: 'id',
-          comment: 'ID of the {{name.camelCase()}}',
-        },
-        title: {
-          type: Sequelize.TEXT,
-          allowNull: false,
-          field: 'title',
-          comment: 'Title of the {{name.camelCase()}}',
-        },
-        description: {
-          type: Sequelize.TEXT,
-          allowNull: true,
-          field: 'description',
-          comment: 'Description of the {{name.camelCase()}}',
-        },
-        createdAt: {
-          type: Sequelize.DATE,
-          allowNull: false,
-          field: 'created_at',
-          comment: "Date and time of the {{name.camelCase()}}'s creation date",
-        },
-        updatedAt: {
-          type: Sequelize.DATE,
-          allowNull: true,
-          field: 'updated_at',
-          comment: "Date and time of the {{name.camelCase()}}'s last update",
-        },
       },
       {
         charset: 'utf8',
